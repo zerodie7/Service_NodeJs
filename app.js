6149db68f2fe00cd95735b3a8013cccde212c7c4
Diego <dmartineza@xcaret.com>
2020-12-16 01:18:30 -0500

    Back door to implementation server in Node.js

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) =>{
-    console.log(req);
+    const url = req.url;
+    const method = req.method;
+
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head><title>Enter message</title><head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if(url === '/message' && method === 'POST'){
+        const body = [];
+
+        req.on('data', chunk => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+
+        return req.on('end', () =>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt', message, err => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        });
+    }
+
+    res.write('<html>');
+    res.write('<head><title>First Page</title><head>');
+    res.write('<body><h1>Hello Node.js Server</h1></body>');
+    res.write('</html>');
+    return res.end();
 });
 
 server.listen(3000);
